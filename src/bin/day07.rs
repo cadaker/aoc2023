d972af92497b303c239a1059ccbe5e51105a42e6
Christian Adåker <5552387+cadaker@users.noreply.github.com>
2023-12-07 11:01:54 +0100

    Day 07 part 2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -52,16 +52,21 @@ fn parse_input() -> Vec<Hand> {
         .collect()
 }
 
-fn count_hand(hand: &Hand) -> Count {
+fn count_hand(hand: &Hand, jokers: bool) -> (Count, i64) {
+    let mut n_jokers = 0;
     let mut counter = Counter::new();
     for ch in hand.cards.chars() {
-        counter.inc(ch);
+        if jokers && ch == 'J' {
+            n_jokers += 1;
+        } else {
+            counter.inc(ch);
+        }
     }
     counter.counts.sort_by(|p1, p2| {
         p1.0.cmp(&p2.0).reverse()
             .then_with(|| rank_card(p1.1).cmp(&rank_card(p2.1)).reverse())
     });
-    counter.counts
+    (counter.counts, n_jokers)
 }
 
 fn rank_card(card: char) -> i64 {
@@ -83,21 +88,33 @@ fn rank_card(card: char) -> i64 {
     }
 }
 
-fn rank_hand(hand: &Hand) -> HandType {
-    let counts = count_hand(hand);
+fn rank_card_jokers(card: char) -> i64 {
+    if card == 'J' {
+        1
+    } else {
+        rank_card(card)
+    }
+}
+
+fn rank_hand(hand: &Hand, jokers: bool) -> HandType {
+    let (counts, n_jokers) = count_hand(hand, jokers);
+    if n_jokers == 5 {
+        return FiveOfAKind;
+    }
+
     let (max_n, _) = counts[0];
-    if max_n == 5 {
+    if max_n + n_jokers == 5 {
         FiveOfAKind
-    } else if max_n == 4 {
+    } else if max_n + n_jokers == 4 {
         FourOfAKind
-    } else if max_n == 3 {
+    } else if max_n + n_jokers == 3 {
         let (second_n, _) = counts[1];
         return if second_n == 2 {
             FullHouse
         } else {
             ThreeOfAKind
         }
-    } else if max_n == 2 {
+    } else if max_n + n_jokers == 2 {
         let (second_n, _) = counts[1];
         return if second_n == 2 {
             TwoPair
@@ -109,9 +126,10 @@ fn rank_hand(hand: &Hand) -> HandType {
     }
 }
 
+
 fn compare_hands(h1: &Hand, h2: &Hand) -> Ordering {
-    let type1 = rank_hand(h1);
-    let type2 = rank_hand(h2);
+    let type1 = rank_hand(h1, false);
+    let type2 = rank_hand(h2, false);
     type1.cmp(&type2)
         .then_with(|| {
             let v1: Vec<i64> = h1.cards.chars().map(rank_card).collect();
@@ -120,14 +138,30 @@ fn compare_hands(h1: &Hand, h2: &Hand) -> Ordering {
         })
 }
 
+fn compare_hands_jokers(h1: &Hand, h2: &Hand) -> Ordering {
+    let type1 = rank_hand(h1, true);
+    let type2 = rank_hand(h2, true);
+    type1.cmp(&type2)
+        .then_with(|| {
+            let v1: Vec<i64> = h1.cards.chars().map(rank_card_jokers).collect();
+            let v2: Vec<i64> = h2.cards.chars().map(rank_card_jokers).collect();
+            v1.cmp(&v2)
+        })
+}
+
+fn winnings(hands: &[Hand]) -> i64 {
+    hands.iter()
+        .enumerate()
+        .map(|(i, hand)| ((i+1) as i64) * hand.bid)
+        .sum()
+}
+
 fn main() {
     let mut hands = parse_input();
 
     hands.sort_by(|h1, h2| compare_hands(h1, h2));
+    println!("{}", winnings(&hands));
 
-    let sum: i64 = hands.iter()
-        .enumerate()
-        .map(|(i, hand)| ((i+1) as i64) * hand.bid)
-        .sum();
-    println!("{}", sum);
+    hands.sort_by(|h1, h2| compare_hands_jokers(h1, h2));
+    println!("{}", winnings(&hands));
 }
