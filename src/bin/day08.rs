ac35eaf4eb0387bba9abb16081e7170c161ae5ea
Christian Adåker <5552387+cadaker@users.noreply.github.com>
2023-12-08 21:05:52 +0100

    Day 08 part 2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -15,20 +15,45 @@ fn read_map(map_lines: &[String]) -> Map {
     ret
 }
 
-fn follow(instructions: &str, map: &Map) -> usize {
+fn follow(instructions: &str, map: &Map, start: &str) -> usize {
     let mut count = 0;
-    let mut pos = &String::from("AAA");
+    let mut pos = &String::from(start);
 
-    while pos != "ZZZ" {
-        if instructions.chars().nth(count % instructions.len()).unwrap() == 'L' {
+    while !pos.ends_with("Z") {
+        let dir: char = instructions.chars().nth(count % instructions.len()).unwrap();
+        if dir == 'L' {
             pos = &map.get(pos).unwrap().0;
         } else {
             pos = &map.get(pos).unwrap().1;
         }
         count += 1;
     }
-
     count
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while a > 0 {
+        (a, b) = (b % a, a);
+    }
+    b
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::gcd;
+    #[test]
+    fn test_gcd() {
+        assert_eq!(gcd(0, 0), 0);
+        assert_eq!(gcd(0, 3), 3);
+        assert_eq!(gcd(3, 3), 3);
+        assert_eq!(gcd(3, 3), 3);
+        assert_eq!(gcd(15, 18), 3);
+        assert_eq!(gcd(77, 19), 1);
+    }
 }
 
 fn main() {
@@ -40,5 +65,12 @@ fn main() {
     let instructions: String = lines[0].clone();
     let map = read_map(&lines[2..]);
 
-    println!("{}", follow(&instructions, &map));
+    println!("{}", follow(&instructions, &map, "AAA"));
+
+    let starts: Vec<String> = map.keys().filter(|&s| s.ends_with("A")).cloned().collect();
+
+    // We can make a simplifying assumption, that each start only leads to one single ending
+    let lengths: Vec<usize> = starts.iter().map(|s| follow(&instructions, &map, s)).collect();
+
+    println!("{}", lengths.iter().cloned().reduce(lcm).unwrap())
 }
