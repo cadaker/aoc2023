38dfa7397f867d61d1220bc1fb2b02d40f40e7b0
Christian Adåker <5552387+cadaker@users.noreply.github.com>
2023-12-15 11:04:34 +0100

    Day 15 part 2

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,9 +1,11 @@
 use aoc2023::utils::stdio_lines;
 
-fn parse_input() -> Vec<Vec<String>> {
+fn parse_input() -> Vec<String> {
     let mut ret = Vec::new();
     for line in stdio_lines() {
-        ret.push(line.split(",").into_iter().map(String::from).collect());
+        for part in line.split(",") {
+            ret.push(String::from(part));
+        }
     }
     ret
 }
@@ -16,14 +18,63 @@ fn hash(s: &str) -> i64 {
     h
 }
 
+fn set_lens(boxes: &mut [Vec<(String, i64)>], label: &str, val: i64) {
+    let boxno = hash(label);
+    let lenses = &mut boxes[boxno as usize];
+    for (i, (lbl, _)) in lenses.iter().enumerate() {
+        if lbl == label {
+            lenses[i] = (String::from(label), val);
+            return;
+        }
+    }
+    lenses.push((String::from(label), val));
+}
+
+fn remove_lens(boxes: &mut [Vec<(String, i64)>], label: &str) {
+    let boxno = hash(label);
+    let lenses = &mut boxes[boxno as usize];
+    for (i, (lbl, _)) in lenses.iter().enumerate() {
+        if lbl == label {
+            lenses.remove(i);
+            return;
+        }
+    }
+}
+
+fn execute(boxes: &mut [Vec<(String, i64)>], oper: &str) {
+    if oper.ends_with("-") {
+        remove_lens(boxes, &oper[0..oper.len()-1]);
+    } else {
+        let parts: Vec<&str> = oper.split("=").collect();
+        let (label, lens) = (parts[0], parts[1]);
+        set_lens(boxes, label, lens.parse().unwrap());
+    }
+}
+
+fn focusing_power(boxes: &Vec<(String, i64)>, boxno: i64) -> i64 {
+    boxes.iter()
+        .enumerate()
+        .map(|(i, (_, val))| (boxno + 1) * (i as i64 + 1) * val)
+        .sum::<i64>()
+}
+
 fn main() {
     let input = parse_input();
     println!("{}",
         input.iter()
-            .map(|vec|
-                vec.iter()
-                    .map(|s| hash(s.as_str()))
-                    .sum::<i64>())
+            .map(|s| hash(s.as_str()))
             .sum::<i64>()
+    );
+
+    let mut boxes = Vec::new();
+    boxes.resize(256, Vec::new());
+    for oper in &input {
+        execute(&mut boxes, oper);
+    }
+
+    println!("{}", boxes.iter()
+        .enumerate()
+        .map(|(i, b)| focusing_power(b, i as i64))
+        .sum::<i64>()
     )
 }
