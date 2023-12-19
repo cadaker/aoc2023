b68e2798fa08ee0a0c04643d0552e97ed8623fb9
Christian Adåker <5552387+cadaker@users.noreply.github.com>
2023-12-19 18:49:11 +0100

    Day 19 part 2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -9,7 +9,7 @@ enum Decision {
     Forward(String),
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Copy, Clone)]
 enum Op {
     LT,
     GT,
@@ -153,6 +153,86 @@ fn resolve(workflows: &HashMap<String, Workflow>, part: &Part) -> Decision {
     }
 }
 
+#[derive(Clone)]
+struct PartsRange {
+    minx: i64,
+    maxx: i64,
+    minm: i64,
+    maxm: i64,
+    mina: i64,
+    maxa: i64,
+    mins: i64,
+    maxs: i64,
+}
+
+fn empty(range: &PartsRange) -> bool {
+    !(range.minx <= range.maxx && range.minm <= range.maxm && range.mina <= range.maxa && range.mins <= range.maxs)
+}
+
+fn cut_range(range: &PartsRange, var: &str, op: Op, limit: i64) -> (PartsRange, PartsRange) {
+    let PartsRange {minx, maxx, minm, maxm, mina, maxa, mins, maxs} = *range;
+    match (var, op) {
+        ("x", Op::LT) => (PartsRange{ minx, maxx: limit - 1, minm, maxm, mina, maxa, mins, maxs },
+                          PartsRange{ minx: limit, maxx, minm, maxm, mina, maxa, mins, maxs }),
+        ("x", Op::GT) => (PartsRange{ minx: limit + 1, maxx, minm, maxm, mina, maxa, mins, maxs },
+                          PartsRange{ minx, maxx: limit, minm, maxm, mina, maxa, mins, maxs }),
+        ("m", Op::LT) => (PartsRange{ minx, maxx, minm, maxm: limit - 1, mina, maxa, mins, maxs },
+                          PartsRange{ minx, maxx, minm: limit, maxm, mina, maxa, mins, maxs }),
+        ("m", Op::GT) => (PartsRange{ minx, maxx, minm: limit + 1, maxm, mina, maxa, mins, maxs },
+                          PartsRange{ minx, maxx, minm, maxm: limit, mina, maxa, mins, maxs }),
+        ("a", Op::LT) => (PartsRange{ minx, maxx, minm, maxm, mina, maxa: limit - 1, mins, maxs },
+                          PartsRange{ minx, maxx, minm, maxm, mina: limit, maxa, mins, maxs }),
+        ("a", Op::GT) => (PartsRange{ minx, maxx, minm, maxm, mina: limit + 1, maxa, mins, maxs },
+                          PartsRange{ minx, maxx, minm, maxm, mina, maxa: limit, mins, maxs }),
+        ("s", Op::LT) => (PartsRange{ minx, maxx, minm, maxm, mina, maxa, mins, maxs: limit - 1 },
+                          PartsRange{ minx, maxx, minm, maxm, mina, maxa, mins: limit, maxs }),
+        ("s", Op::GT) => (PartsRange{ minx, maxx, minm, maxm, mina, maxa, mins: limit + 1, maxs },
+                          PartsRange{ minx, maxx, minm, maxm, mina, maxa, mins, maxs: limit }),
+        _ => panic!("Bad range cut"),
+    }
+}
+
+fn combination_count(range: &PartsRange) -> i64 {
+    (range.maxx - range.minx + 1) *
+        (range.maxm - range.minm + 1) *
+        (range.maxa - range.mina + 1) *
+        (range.maxs - range.mins + 1)
+}
+
+fn combinations(workflows: &HashMap<String, Workflow>, flow: &String, range: &PartsRange) -> i64 {
+    let mut remaining_range = range.clone();
+
+    let workflow = workflows.get(flow).unwrap();
+    let mut ret = 0;
+    for rule in &workflow.rules {
+        if let Rule::Finish(Decision::Accept) = rule {
+            ret += combination_count(&remaining_range);
+            break;
+        } else if let Rule::Finish(Decision::Reject) = rule {
+            break;
+        } else if let Rule::Finish(Decision::Forward(next)) = rule {
+            ret += combinations(workflows, next, &remaining_range);
+            break;
+        } else if let Rule::Branch(BranchRule{ var, op, limit, decision}) = rule {
+            let (true_branch, false_branch) = cut_range(&remaining_range, var, *op, *limit);
+            if !empty(&true_branch) {
+                if let Decision::Accept = decision {
+                    ret += combination_count(&true_branch);
+                } else if let Decision::Forward(next) = decision {
+                    ret += combinations(workflows, next, &true_branch);
+                }
+            }
+            if empty(&false_branch) {
+                break;
+            }
+            remaining_range = false_branch;
+        } else {
+            panic!("Bad rule");
+        }
+    }
+    ret
+}
+
 fn main() {
     let (workflows, parts) = parse_input();
 
@@ -161,4 +241,12 @@ fn main() {
         .cloned()
         .collect();
     println!("{}", accepted.iter().map(|p| p.x + p.m + p.a + p.s).sum::<i64>());
+
+    let range = PartsRange{
+        minx: 1, maxx: 4000,
+        minm: 1, maxm: 4000,
+        mina: 1, maxa: 4000,
+        mins: 1, maxs: 4000,
+    };
+    println!("{}", combinations(&workflows, &String::from("in"), &range));
 }
