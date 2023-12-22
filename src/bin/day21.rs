b5de15fc9806ae52affa6db3e2be37014e9aa2b8
Christian Adåker <5552387+cadaker@users.noreply.github.com>
2023-12-22 03:19:35 +0100

    Day 21 part 2

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,7 +1,7 @@
 use aoc2023::utils::stdio_lines;
 use aoc2023::grid::{Grid, GridBuilder, Point};
 use aoc2023::dir::cart_neighbours;
-use std::collections::VecDeque;
+use std::collections::{VecDeque, HashSet};
 
 fn parse_input() -> (Grid<char>, Point) {
     let mut builder = GridBuilder::new();
@@ -21,33 +21,73 @@ fn parse_input() -> (Grid<char>, Point) {
     (builder.finish(), start)
 }
 
-fn search_reachable(grid: &Grid<char>, start: &Point, max_steps: usize) -> usize {
-    let mut visited = Grid::from_dim(grid.height(), grid.width(), false);
+struct WrappedGrid {
+    grid: Grid<char>
+}
+
+fn safe_mod(x: i32, n: i32) -> i32 {
+    ((x % n) + n) % n
+}
+impl WrappedGrid {
+    fn new(grid: Grid<char>) -> WrappedGrid {
+        WrappedGrid{ grid }
+    }
+
+    fn getp(&self, p: &Point) -> &char {
+        self.grid.get(safe_mod(p.row, self.grid.height()), safe_mod(p.col, self.grid.width()))
+    }
+}
+
+fn search_reachable(grid: &WrappedGrid, start: &Point, max_steps: usize) -> Vec<usize> {
+    let mut ret = Vec::new();
+    let mut visited: HashSet<Point> = HashSet::new();
     let mut queue = VecDeque::new();
     queue.push_back((*start, 0usize));
-    *visited.mutgetp(start) = true;
-    let mut count = 0usize;
+    visited.insert(*start);
 
     while !queue.is_empty() {
         let (pos, steps) = queue.pop_front().unwrap();
-        if (steps % 2) == 0 {
-            count += 1;
+        if (steps % 2) == (max_steps % 2) {
+            if steps / 2 == ret.len() {
+                ret.push(1);
+            } else {
+                ret[steps / 2] += 1;
+            }
         }
         if steps < max_steps {
             for n in cart_neighbours(&pos) {
-                if visited.containsp(&n) && !*visited.getp(&n) && *grid.getp(&n) == '.' {
+                if !visited.contains(&n) && *grid.getp(&n) == '.' {
                     queue.push_back((n, steps + 1));
-                    *visited.mutgetp(&n) = true;
+                    visited.insert(n);
                 }
             }
         }
     }
 
-    count
+    ret
+}
+
+fn predict_output(grid: &WrappedGrid, start: &Point, max_steps: usize) -> usize {
+    let n = (max_steps + 1) / 2;
+    const PERIOD: usize = 131; // Actually half of the "real" period.
+
+    let loops = n / PERIOD - 1;
+    let offset = (n % PERIOD) + PERIOD;
+
+    let x = search_reachable(&grid, &start, 1200 + (max_steps % 2));
+
+    // The data in x grows as x_i+P = x_i + delta_i, or x_i+rP = x_i + r*delta_i
+    // We want the sum of the first n terms of x.
+    x[0..offset].iter().sum::<usize>() +
+        loops * x[offset..offset + PERIOD].iter().sum::<usize>() +
+        (loops - 1) * loops / 2 * ((offset..offset + PERIOD).map(|i| x[i + 2*PERIOD] - x[i + PERIOD]).sum::<usize>())
 }
 
 fn main() {
     let (grid, start) = parse_input();
+    let wrapped_grid = WrappedGrid::new(grid);
 
-    println!("{}", search_reachable(&grid, &start, 64));
+    println!("{}", search_reachable(&wrapped_grid, &start, 64).iter().sum::<usize>());
+    const N: usize = 26501365;
+    println!("{}", predict_output(&wrapped_grid, &start, N));
 }
